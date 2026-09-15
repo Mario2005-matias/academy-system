94e76d03c1a20fc222f634e5b95a3804c54b1ade
matiasDev007 <159073520+Mario2005-matias@users.noreply.github.com>
2026-09-15 19:23:19 +0100

    Criei as migration

---
+++ database/migrations/2026_09_15_173311_create_payments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('enrollment_id')->constrained('enrollments')->onDelete('cascade');
+            $table->decimal('value', 8, 2);
+            $table->date('payment_date');
+            $table->enum('payment_method', ['TPA', 'Transferencia', 'Express'])->default('Express');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
