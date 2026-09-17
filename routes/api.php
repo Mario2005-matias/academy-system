14cb992532de3e01987ab60d39d56210362fc92d
matiasDev007 <159073520+Mario2005-matias@users.noreply.github.com>
2026-09-17 08:39:49 +0100

    Trabalhando o CRUD do aluno

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\V1\Api\AuthController;
+use App\Http\Controllers\V1\Api\CheckInController;
+use App\Http\Controllers\V1\Api\EnrollementController;
+use App\Http\Controllers\V1\Api\PaymentController;
+use App\Http\Controllers\V1\Api\PlanController;
+use App\Http\Controllers\V1\Api\ReportController;
+use App\Http\Controllers\V1\Api\StudentController;
+use App\Http\Controllers\V1\Api\TeacherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,16 +19,16 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', [AuthController::class, 'me']);
 
     Route::middleware('role:administrador,recepcionista')->group(function () {
-        // Route::apiResource('alunos', AlunoController::class);
-        // Route::post('/matriculas', [MatriculaController::class, 'store']);
-        // Route::post('/pagamentos', [PagamentoController::class, 'store']);
-        // Route::post('/checkins', [CheckinController::class, 'store']);
+        Route::get('/students', [StudentController::class, 'index'])->name('students.index');
+        Route::post('/enrollments', [EnrollementController::class, 'store'])->name('enrollments.store');
+        Route::post('/payments', [PaymentController::class, 'store'])->name('payments.store');
+        Route::post('/checkins', [CheckInController::class, 'store'])->name('checkins.store');
     });
 
     Route::middleware('role:administrador')->group(function () {
-        // Route::apiResource('planos', PlanoController::class)->except(['index', 'show']);
-        // Route::delete('/pagamentos/{id}', [PagamentoController::class, 'destroy']);
-        // Route::apiResource('professores', ProfessorController::class);
-        // Route::get('/dashboard', [DashboardController::class, 'index']);
+        Route::get('/plans', [PlanController::class, 'index'])->name('plans.index');
+        Route::delete('/payments/{id}', [PaymentController::class, 'destroy']);
+        Route::apiResource('teachers', TeacherController::class);
+        Route::get('/reports', [ReportController::class, 'index']);
     });
 });
